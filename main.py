ff8c658507b47af789bfbd389f3a2ba21ee30a24
Jerry <js843@njit.edu>
2020-10-09 17:27:30 -0400

    Added: python flask backend

---
+++ main.py
@@ -0,0 +1,17 @@
+import flask 
+import os
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def index():
+    return flask.render_template(
+        "index.html"
+    )
+
+if (__name__=="__main__"):
+    app.run(
+        debug=True,
+        port=int(os.getenv("PORT", 8080)),
+        host=os.getenv("IP", "0.0.0.0")
+    )
