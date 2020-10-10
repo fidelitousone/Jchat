bbf3824d59f24afa508edce4324e70dc7683e7ca
Jerry <js843@njit.edu>
2020-10-09 23:58:16 -0400

    enable a very basic version of flask socket.io

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import flask 
 import os
+from flask_socketio import SocketIO
 
 app = flask.Flask(__name__)
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
 
 @app.route("/")
 def index():
@@ -9,9 +12,18 @@ def index():
         "index.html"
     )
 
+@socketio.on('connect')
+def on_connect():
+    print("User connected to the server.")
+    socketio.emit('connected', {
+        'test': 'Connected'
+    })
+
+
 if (__name__=="__main__"):
-    app.run(
+    socketio.run(
+        app,
         debug=True,
         port=int(os.getenv("PORT", 8080)),
-        host=os.getenv("IP", "localhost")
+        host=os.getenv("IP", "127.0.0.1")
     )
