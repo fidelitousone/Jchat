9719abaa0a485df8130723c4e2473233d8a3e68d
Jerry <js843@njit.edu>
2020-10-09 21:47:37 -0400

    add message history in chat box with proper margins

---
+++ scripts/MessageHistory.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './myStyle.css';
+
+export default function MessageHistory() {
+    return (
+        <div className="MessageHistory">
+            
+        </div>
+    )
+}
