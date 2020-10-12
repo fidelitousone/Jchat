3caf7ca3c7305a7f8cf157b398a048c0f96f3093
Jerry <js843@njit.edu>
2020-10-12 14:11:38 -0400

    refactor the models.py file

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
-import flask_sqlalchemy
 from main import db
+
 
 class Messages(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120))
     message = db.Column(db.String(1000))
-    
+
     def __init__(self, u, m):
         self.username = u
         self.message = m
-        
+
     def __repr__(self):
         return f"<Message Data: {self.username}, {self.message}>"
