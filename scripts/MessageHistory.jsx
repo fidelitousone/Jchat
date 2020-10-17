647c0bb0aa74da7c6c1249e935bb01e9887b34f4
Jatinder Singh <js843@njit.edu>
2020-10-17 02:26:08 +0000

    add very basic URL detection to react

--- scripts/MessageHistory.jsx
+++ scripts/MessageHistory.jsx
@@ -1,23 +1,46 @@
 import * as React from 'react';
 import './myStyle.css';
 import { Socket } from './Socket';
+var URL = require('url').URL;
 
 export default function MessageHistory() {
     const [messageList, setMessage] = React.useState([]);
+    var regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+    
+    function check_url(message) {
+        try {
+            new URL(message)
+        } catch (_) {
+            return false;
+        }
+        return true;
+    }
+    
     function new_messages() {
         React.useEffect(() => {
+            // Fills all the messages in the state from the database
             Socket.on("message receieved", (data) => {
                 setMessage(data["messages"]);
             });
+            
         }, []);
     }
-
+    
     new_messages();
-
+    
+    for (var message of messageList) {
+        var message_without_user = message.split("User: ")[1]
+        if (regex.test(message_without_user)) {
+            console.log("This is a URL");
+        } else {
+            console.log("This is not a URL")
+        }
+    }
+    
     return (
         <div className="MessageHistory">
-            {messageList.map(messageList => (
-                <p key={messageList}>{messageList}</p>
+            {messageList.map(message => (
+                <p key={message}>{message}</p>
             ))}
         </div>
     );
