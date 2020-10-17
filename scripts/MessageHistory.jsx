ba7c65e4aa6a69c44a5df219ecc0f6184f44d051
Jerry <js843@njit.edu>
2020-10-17 15:55:23 -0400

    Render images users send inline with chat

--- scripts/MessageHistory.jsx
+++ scripts/MessageHistory.jsx
@@ -8,6 +8,7 @@ import MessageImage  from './MessageImage';
 export default function MessageHistory() {
     const [messageList, setMessage] = React.useState([]);
     var regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+    var image_regex = /^https?:\/\/(?:[a-z0-9\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png|bmp)$/i
 
     function list_messages() {
         var arry = [];
@@ -18,7 +19,9 @@ export default function MessageHistory() {
                 var message_without_user = message.split("User: ")[1]
             }
             var user = message.split(":")[0]
-            if (regex.test(message_without_user)) {
+            if (image_regex.test(message_without_user)) {
+                arry.push(<MessageImage username={user} image_url={message_without_user} />);
+            } else if (regex.test(message_without_user)) {
                 arry.push(<MessageUrl username={user} message={message_without_user} />);
             } else {
                 arry.push(<Message username={user} message={message_without_user} />);
--- scripts/MessageImage.jsx
+++ scripts/MessageImage.jsx
@@ -4,7 +4,7 @@ export default function MessageImage(props) {
 
     return (
         <div className="MessageImage">
-
+            <p>{props.username}: <img src={props.image_url} width="250" height="250"></img></p>
         </div>
     )
 }
